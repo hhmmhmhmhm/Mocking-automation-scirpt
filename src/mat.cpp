adc5c255a56a9f99f3a2c1cbb802573ad431e9ef
Mathieu <mathieukerbel@gmail.com>
2024-01-23 17:50:23 +0100

    Add performance measurement framework.

--- src/mat.cpp
+++ src/mat.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+/****************************************************************************************
+* 
+* Credit : Mathieu KERBEL (Consultant) - Mideky-Hub
+* 
+* Description :
+*	The goal is to give developper a built-in tool to allow them to automate endpoint test
+*	with Postman API.
+* 
+* Assistance :
+*	If you need any assistance of any form to use this API tool, ask @Mathieu Kerbel on Teams,
+*	Else make a ticket on GitHub.
+****************************************************************************************/
+
 int main(void) {
 	return 0;
 }
